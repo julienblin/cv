7118e61a9463addaeeaa2b87d72c21d6b58d14f6
Julien Blin <julien.blin@gmail.com>
2010-02-26 11:33:17 -0500

    Added rough multi-language support.

--- Sources/html_transformer.rb
+++ Sources/html_transformer.rb
@@ -14,12 +14,16 @@ class HtmlTransformer < BaseTransformer
 		
 		template = File.read('Sources/html_templates/index.haml')
 		haml_engine = Haml::Engine.new(template)
-		output = haml_engine.render(Object.new, :cv => @data)
-		File.open(@output_dir + '/index.html', 'w') do |f|
-			f.write(output)
-		end
 		
-		FileUtils.copy('Sources/html_templates/cvstyle.css', @output_dir)
+		[:en, :fr].each do |lang|
+		  resources = YamlParser.new('Data/resources.yml').read.resources.send(lang)
+		  output = haml_engine.render(Object.new, :cv => @data, :lang => lang, :resources => resources)
+  		File.open(@output_dir + "/index.#{lang}.html", 'w') do |f|
+  			f.write(output)
+  		end
+
+  		FileUtils.copy('Sources/html_templates/cvstyle.css', @output_dir)
+		end
 	end
 	
 end
