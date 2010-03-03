2aa428ed0c8b8407de4bf336587a8e1220238fcb
Julien Blin <julien.blin@gmail.com>
2010-03-03 17:52:24 -0500

    Added odt skills.

--- Sources/odt_transformer.rb
+++ Sources/odt_transformer.rb
@@ -25,6 +25,15 @@ class Date
   end
 end
 
+class Array
+  def in_groups_of(number, fill_with = nil, &block) 
+    require 'enumerator' 
+    collection = dup 
+    collection << fill_with until collection.size.modulo(number).zero? 
+    collection.each_slice(number, &block) 
+  end
+end
+
 class OdtTransformer < BaseTransformer
   include Serenity::Generator
 
