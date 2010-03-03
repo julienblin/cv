c90c8db6890fdb2c03199dec44354ff5af16ef6c
Julien Blin <julien.blin@gmail.com>
2010-03-03 18:18:32 -0500

    Integrated odt in html template.

--- Sources/html_transformer.rb
+++ Sources/html_transformer.rb
@@ -27,6 +27,7 @@ class HtmlTransformer < BaseTransformer
   		FileUtils.copy('Sources/html_templates/cvstyle.css', @output_dir)
   		FileUtils.copy('Sources/html_templates/.htaccess', @output_dir)
   		FileUtils.copy('Sources/html_templates/robots.txt', @output_dir)
+  		FileUtils.copy('Sources/html_templates/odt.png', @output_dir)
 		end
 	end
 	
