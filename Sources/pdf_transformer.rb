e023f53b6fe313dc21a652d1da4ef7a7404284e7
Julien Blin <julien.blin@gmail.com>
2010-09-08 17:59:39 -0400

    Added pdf conversion using wkhtmltopdf. Removed previous odt generator.

--- Sources/pdf_transformer.rb
+++ Sources/pdf_transformer.rb
@@ -2,34 +2,23 @@ require 'Sources/base_transformer'
 
 class PdfTransformer < BaseTransformer
   
-  def initialize(output_dir, odt_output_dir)
+  def initialize(cv, output_dir, html_output_dir)
+		@cv = cv
 		@output_dir = output_dir
-		@odt_output_dir = odt_output_dir
+		@html_output_dir = html_output_dir
 	end
 
 	def transform
 		wipe_output_dir
-		start_soffice_server
-		Dir.glob("#{@odt_output_dir}/*.odt").each do |file|
-		  odt_file_name = File.basename(file)
-		  pdf_file_name = "#{odt_file_name[0, odt_file_name.size - 4]}.pdf"
-		  puts "Converting #{odt_file_name} to #{pdf_file_name}..."
-		  `java -jar Bin/jodconverter-2.2.2/lib/jodconverter-cli-2.2.2.jar "#{file}" "#{@output_dir}/#{pdf_file_name}"`
-	  end
-		stop_soffice_server
-	end
-	
-	def start_soffice_server
-	  puts "Starting OpenOffice server..."
-	  Thread.new do
-      @sofficepipe = IO.popen("/Applications/OpenOffice.org.app/Contents/MacOS/soffice.bin -headless -accept=\"socket,host=127.0.0.1,port=8100;urp;\" -nofirststartwizard")
+		
+		langs = [:en, :fr]
+    langs.each do |lang|
+      @resources = YamlParser.new('Data/resources.yml').read.resources.send(lang)
+      html_file = @html_output_dir + "/index.#{lang}.html"
+      pdf_file_name = @output_dir + "/#{@resources.cv}-#{@cv.name}.#{lang}.pdf"
+		  
+		  `Bin/wkhtmltopdf --print-media-type --page-size Letter --title "#{@resources.cv} #{@cv.name}" "#{html_file}" "#{pdf_file_name}"`
     end
-    sleep 5
-	end
-	
-	def stop_soffice_server
-	  puts "Terminating OpenOffice server (pid #{@sofficepipe.pid})..."
-	  Process.kill("QUIT", @sofficepipe.pid.to_i)
 	end
   
 end
